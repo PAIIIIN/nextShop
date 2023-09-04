ffb3ef22dd084a9d68a84e9175db5725ff6363e7
S1lence <altynbekernur@gmail.com>
2023-09-04 22:03:31 +0600

    added category with react select

--- app/components/Products.tsx
+++ app/components/Products.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import Image from "next/image";
 import { getProducts } from "../services/data";
 import { useQuery, useQueryClient } from "react-query";
@@ -5,13 +6,25 @@ import { IProducts } from "../interface/data.interface";
 import { FC } from "react";
 import { useDispatch } from "react-redux";
 import { actions } from "../store/cart/cart.slice";
-import { AiFillEye } from "react-icons/ai";
-import { AiOutlinePlus } from "react-icons/ai";
+import { AiFillEye, AiOutlinePlus } from "react-icons/ai";
+import ReactSelect from "react-select";
+
+interface ICategoryOption {
+  value: string;
+  label: string;
+}
 
 export const Products: FC = () => {
   const queryClient = useQueryClient();
-  const { data } = useQuery(["products"], getProducts);
+  const { data } = useQuery<IProducts[]>(["products"], getProducts);
   const dispatch = useDispatch();
+  const [selectedCategory, setSelectedCategory] =
+    useState<ICategoryOption | null>(null);
+
+  const categoryOptions: ICategoryOption[] = [
+    { value: "men's clothing", label: "Men's Clothing" },
+    { value: "women's clothing", label: "Women's Clothing" },
+  ];
 
   const handleAddToCart = (
     id: number,
@@ -25,14 +38,30 @@ export const Products: FC = () => {
 
   return (
     <div className="container mx-auto mt-8 p-4">
+      <div className="mb-4">
+        <label htmlFor="category" className="font-semibold text-gray-600">
+          Select Category:
+        </label>
+        <ReactSelect
+          id="category"
+          options={categoryOptions}
+          value={selectedCategory}
+          onChange={(selectedOption) => setSelectedCategory(selectedOption)}
+          isClearable
+        />
+      </div>
       {data ? (
         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 max-w-screen-xl mx-auto">
           {data
-            .filter(
-              (product: IProducts) =>
+            .filter((product: IProducts) => {
+              if (selectedCategory) {
+                return product.category === selectedCategory.value;
+              }
+              return (
                 product.category === "men's clothing" ||
                 product.category === "women's clothing"
-            )
+              );
+            })
             .map((product: IProducts) => {
               return (
                 <div
@@ -75,7 +104,7 @@ export const Products: FC = () => {
         </div>
       ) : (
         <h1 className="text-2xl font-semibold text-center mt-8">
-          Data Not Found
+          Loading Data...
         </h1>
       )}
     </div>
